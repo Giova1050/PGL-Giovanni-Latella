14fcd47badf445726be9aa0ab102ceb9bed7a5e3
Giovanni <usr3855@salesianos-lacuesta.net>
2024-11-06 10:25:36 +0000

    colors added

---
+++ PGL-Giovanni/styles/colors.ts
@@ -0,0 +1,16 @@
+export const LIGHTTHEME = {
+    azuloscuro: "#1a598f",
+    amarillo: "#ffd700",
+    azulclaro: "#1a65a9",
+    azulmarino: "#234e98",
+    verde: "#0f9f52"
+}
+
+export const DARKTHEME = {
+    beige: "#e4d4ba",
+    grisclaro: "#bbab94",
+    gris: "#827261",
+    grisoscuro: "#524034",
+    negro: "#110906"
+
+}
