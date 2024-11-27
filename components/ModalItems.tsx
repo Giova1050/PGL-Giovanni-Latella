5bfc70415cec8013f4ee9f2e06aacf1a092a0324
Giovanni <usr3855@salesianos-lacuesta.net>
2024-11-27 13:04:38 +0000

    feat: created component modalItems

---
+++ components/ModalItems.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ModalItems = () => {
+  return (
+    <View>
+      <Text>ModalItems</Text>
+    </View>
+  )
+}
+
+export default ModalItems
+
+const styles = StyleSheet.create({})
