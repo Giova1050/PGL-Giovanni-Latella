06e5bd592396b49223f36353dc93c4e741baf1d0
Giovanni <usr3855@salesianos-lacuesta.net>
2025-01-21 21:47:49 +0000

    Drawer created and render manager provider created

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,49 +1,44 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { Drawer } from 'expo-router/drawer';
+import { Drawer } from "expo-router/drawer";
 
 const Layout = () => {
-
-    return (
-
-        <GestureHandlerRootView >
-                <View style={{ flex: 1 }}>
-                    <Drawer
-                        screenOptions={{
-                            drawerStyle: { maxHeight: "100%" },
-                        }}
-                    >
-                        <Drawer.Screen
-                            name="welcome/welcomePage"
-                            options={{
-                                drawerLabel: "Bienvenida",
-                                title: "Bienvenida",
-                            }}
-                        />
-                        <Drawer.Screen
-                            name="portfolio"
-                            options={{
-                                drawerLabel: "Portafolio Personal",
-                                title: "Portfolio",
-                            }}
-                        />
-                        <Drawer.Screen
-                            name="shopping-list/ShoppingList"
-                            options={{
-                                drawerLabel: "Lista de Compra",
-                                title: "Lista de compras",
-                            }}
-                        />
-
-
-                    </Drawer>
-                </View>
-        </GestureHandlerRootView>
-
-    );
+  return (
+    <GestureHandlerRootView>
+      <View style={{ flex: 1 }}>
+        <Drawer
+          screenOptions={{
+            drawerStyle: { maxHeight: "100%" },
+          }}
+        >
+          <Drawer.Screen
+            name="welcome/welcomePage"
+            options={{
+              drawerLabel: "Bienvenida",
+              title: "Bienvenida",
+            }}
+          />
+          <Drawer.Screen
+            name="profile/Profile"
+            options={{
+              drawerLabel: "Portafolio Personal",
+              title: "Portfolio",
+            }}
+          />
+          <Drawer.Screen
+            name="shoppingList/ShoppingList"
+            options={{
+              drawerLabel: "Lista de Compra",
+              title: "Lista de compras",
+            }}
+          />
+        </Drawer>
+      </View>
+    </GestureHandlerRootView>
+  );
 };
 
 export default Layout;
