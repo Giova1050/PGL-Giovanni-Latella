16fe2f62944359820feeb1b6e5a83bd476fca029
Giovanni <usr3855@salesianos-lacuesta.net>
2025-01-26 14:28:02 +0000

    handleLogout added

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
+import asyncStorageService from "../../service/async-storage";
+import { router } from "expo-router";
 
 const DrawerLayout = () => {
+
+  const handleLogout = async () => {
+    try {
+        await asyncStorageService.removeData("token");
+        router.navigate("/login/LoginPage");
+    } catch (error) {
+        console.error('Error al cerrar sesión:', error);
+    }
+};
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
