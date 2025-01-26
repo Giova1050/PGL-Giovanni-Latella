e5acc10c50d4702c4a1983ffa3e27fe80720bf0d
Giovanni <usr3855@salesianos-lacuesta.net>
2025-01-26 14:21:40 +0000

    token added

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,24 @@
 import { useEffect } from "react";
 import { router } from "expo-router";
 import { StyleSheet } from "react-native";
+import asyncStorageService from "../service/async-storage";
 
 export default function App() {
-  /* const USER_TOKEN_KEY = asyncStorageService.KEYS.userToken; */
-  let isUserTokenSaved = false;
+  
 
   useEffect(() => {
     const getSavedUserToken = async () => {
-      // const token = await asyncStorageService.get<string>(USER_TOKEN_KEY);
-      const token = null; // La buena es la anterior, esta es solo para entrar en el drawer
+      const token = await asyncStorageService.getData("token");
       const isTokenValid = true;
       if (token != null && isTokenValid) {
-        isUserTokenSaved = true;
         router.navigate("/(drawer)/welcome-page");
       } else {
         router.navigate("/user-management/login-page");
       }
     };
-
     getSavedUserToken();
   }, []);
+  return
 }
 
 const styles = StyleSheet.create({});
