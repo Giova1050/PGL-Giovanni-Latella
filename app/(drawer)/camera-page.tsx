74697e401ac9214f49fba194b25db4b713ba5406
Giovanni <usr3855@salesianos-lacuesta.net>
2025-02-09 01:30:48 +0000

    class CameraPage created

---
+++ app/(drawer)/camera-page.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+const CameraPage = () => {
+
+  return (
+    <View>
+      <Text>camera-page</Text>
+    </View>
+  )
+}
+
+export default CameraPage
+
+const styles = StyleSheet.create({
+
+
+})
