2624ca19777078cf894b239ce1b75f28886482ae
Giovanni <usr3855@salesianos-lacuesta.net>
2025-02-09 10:41:05 +0000

    camera-page added to drawer

--- app/(drawer)/camera-page.tsx
+++ app/(drawer)/camera-page.tsx
@@ -1,18 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { ImageType } from "../../types/ImageType";
+import asyncStorageService from "../../service/async-storage";
+import { LIGHTTHEME } from "../../styles/colors";
 
 const CameraPage = () => {
+  const [images, setImages] = useState<ImageType[]>([]);
+  const [openCamera, setOpenCamera] = useState(false);
+  const [userToken, setUserToken] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  
 
   return (
-    <View>
-      <Text>camera-page</Text>
+    <View style={styles.container}>
+      
     </View>
-  )
-}
+  );
+};
 
-export default CameraPage
+export default CameraPage;
 
 const styles = StyleSheet.create({
-
-
-})
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: LIGHTTHEME.azulclaro,
+        paddingHorizontal: 50,
+      },
+});
